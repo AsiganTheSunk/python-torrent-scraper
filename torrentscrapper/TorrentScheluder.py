93d5cfa0dff790a203225fbec62469fc0eaaa060
asiganthesunk@gmail.com <asiganthesunk@gmail.com>
2017-09-13 00:56:51 +0200

    Dont know

--- torrentscrapper/TorrentScheluder.py
+++ torrentscrapper/TorrentScheluder.py
@@ -2,6 +2,7 @@
 
 import sched
 import time
+import schedule
 
 scheduler = sched.scheduler(time.time, time.sleep)
 
@@ -13,3 +14,16 @@ scheduler.enter(2, 1, print_event, ('first',))
 scheduler.enter(5, 1, print_event, ('second',))
 
 scheduler.run()
+
+def job():
+    print("I'm working...")
+
+schedule.every(10).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every().day.at("10:30").do(job)
+schedule.every().monday.do(job)
+schedule.every().wednesday.at("13:15").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
