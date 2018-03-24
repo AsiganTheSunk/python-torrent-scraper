00dc69be152367e3cb640c1327fa8b772b84c701
Pedro Arias Ruiz <AsiganTheSunk@users.noreply.github.com>
2018-03-24 01:33:57 +0100

    removed duplicated entries
    removed duplicated entries

--- torrentscrapper/TVCalendar.py
+++
@@ -1,6 +0,0 @@
-class TVCalendar():
-    def __init__(self, main_month_uri):
-        self.main_month_uri = main_month_uri
-        self.next_month_uri = ''
-        self.previous_month_uri = ''
-        self.main_calendar = {}
