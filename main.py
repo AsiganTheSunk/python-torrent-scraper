54b8cff6a3ec41c42b0f157152271ab01121f594
Asigan <asiganthesunk@gmail.com>
2021-04-24 23:02:09 +0200

    main change

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ from core.net.network_agent import NetworkAgent
 from core.tracker_scraper import TrackerScraper
 
 if __name__ == '__main__':
+
     network_agent = NetworkAgent()
     if network_agent.network_status():
         # Move Size Limit to Other Place, not Withing the Search, or apply other
         # filters based on the current place we are searching.
         web_search_instance = WebSearchInstance(
             title='Mortal Kombat',
-            year='2021', quality='1080p',
+            year='1995', quality='1080p',
             search_type=FileFlags.FILM_DIRECTORY_FLAG,
             upper_size_limit='4000', lower_size_limit='1000')
 
@@ -32,3 +33,11 @@ if __name__ == '__main__':
     #
     # with open('./test/static/GranTorrentTorrent.html', encoding='utf-8') as file:
     #     kat_scraper.get_magnet_link(file)
+
+
+    # https://stackoverflow.com/questions/63944480/maxretryerror-selenium
+
+
+    # You can type a list as python 3.8.4
+    # https://docs.python.org/3/library/typing.html
+    # from typing import List
